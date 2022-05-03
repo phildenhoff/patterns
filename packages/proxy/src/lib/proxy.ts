7215e1b2c6e771255207c54706cf81e82e6c3372
Phil Denhoff <phil@denhoff.ca>
2022-05-02 23:08:01 -0700

    Add Proxy implementation

--- packages/proxy/src/lib/proxy.ts
+++ packages/proxy/src/lib/proxy.ts
@@ -1,3 +1,17 @@
-export function proxy(): string {
-  return 'proxy';
-}
+export type DevProxyHandler<T> = {
+  get?: <TProp extends keyof T>(target: T, name: TProp) => T[TProp];
+};
+
+export const DevProxy = <T extends object>(
+  target: T,
+  handler: DevProxyHandler<T>
+): T => {
+  const proxy = { ...target };
+  (Object.keys(target) as (keyof typeof target)[]).forEach((key) => {
+    Object.defineProperty(proxy, key, {
+      get: () => (handler?.get ? handler?.get(target, key) : target[key]),
+    });
+  });
+
+  return proxy;
+};
