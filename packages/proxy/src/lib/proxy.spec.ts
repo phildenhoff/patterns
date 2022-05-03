0248e0432e5d6122429db5d914de3e7e1ec7e0a0
Phil Denhoff <phil@denhoff.ca>
2022-05-02 23:08:01 -0700

    Add Proxy implementation

--- packages/proxy/src/lib/proxy.spec.ts
+++ packages/proxy/src/lib/proxy.spec.ts
@@ -1,7 +1,27 @@
-import { proxy } from './proxy';
+import { DevProxy } from './proxy';
 
-describe('proxy', () => {
-  it('should work', () => {
-    expect(proxy()).toEqual('proxy');
+describe('Proxy', () => {
+  it('should be transparent when provided no handlers', () => {
+    const target = {
+      field: 'value',
+    };
+    const handler = {};
+
+    const proxy = DevProxy(target, handler);
+    expect(proxy.field).toEqual('value');
+  });
+
+  it('should allow the handler to modify fields', () => {
+    const target = {
+      field: 'value',
+    };
+    const handler = {
+      get: (target: Record<string, string>, name: string) => {
+        return target[name].toUpperCase();
+      },
+    };
+
+    const proxy = DevProxy(target, handler);
+    expect(proxy.field).toEqual('VALUE');
   });
 });
